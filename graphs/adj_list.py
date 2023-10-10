98f56343df1284399d911193c36ae7bba2daf143
Andrew Subotin <subotinandrey5@gmail.com>
2023-10-10 16:37:31 +0200

    Create adj_list.py

---
+++ graphs/adj_list.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+# Adjacency List Representation in Python
+
+
+class AdjNode:
+    def __init__(self, value):
+        self.vertex = value
+        self.next: Optional[AdjNode] = None
+
+
+class Graph:
+    def __init__(self, num):
+        self.V = num
+        self.graph = [None] * self.V
+
+    def add_edge(self, src, dest):
+        node = AdjNode(dest)
+        node.next = self.graph[src]
+        self.graph[src] = node
+
+        node = AdjNode(src)
+        node.next = self.graph[dest]
+        self.graph[dest] = node
+
+    def print_graph(self):
+        for i in range(self.V):
+            print("Vertex " + str(i) + ":", end="")
+            temp = self.graph[i]
+            while temp:
+                print(" -> {}".format(temp.vertex), end="")
+                temp = temp.next
+            print(" \n")
+
+
+if __name__ == "__main__":
+    V = 5
+
+    # Create graph and edges
+    graph = Graph(V)
+    graph.add_edge(0, 1)
+    graph.add_edge(0, 4)
+    graph.add_edge(1, 2)
+    graph.add_edge(1, 3)
+    graph.add_edge(1, 4)
+    graph.add_edge(2, 3)
+    graph.add_edge(3, 4)
+
+    graph.print_graph()
