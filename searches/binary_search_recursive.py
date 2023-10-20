19c58a3fa2be0452cdf7cf964b16b02d4bbd480c
Andrew Subotin <subotinandrey5@gmail.com>
2023-10-20 12:06:43 +0200

    Create binary_search_recursive.py

---
+++ searches/binary_search_recursive.py
@@ -0,0 +1,31 @@
+
+# Recursive Binary Search in Python
+
+
+def binary_search(array: list, x: int, low: int, high: int) -> int:
+
+    if high >= low:
+
+        mid = low + (high - low) // 2
+
+        if array[mid] == x:
+            return mid
+
+        elif array[mid] > x:
+            return binary_search(array, x, low, mid - 1)
+
+        else:
+            return binary_search(array, x, mid + 1, high)
+
+    return -1
+
+
+array = [3, 4, 5, 6, 7, 8, 9]
+x = 4
+
+result = binary_search(array, x, 0, len(array)-1)
+
+if result != -1:
+    print("Element is present at index " + str(result))
+else:
+    print("Not found")
