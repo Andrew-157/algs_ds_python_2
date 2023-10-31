fe32c03573a1925b4ad7226af5f410e2fcc5c307
Andrew Subotin <subotinandrey5@gmail.com>
2023-10-31 12:59:53 +0100

    feat: dijkstra algorithm implementation

---
+++ greedy_algs/dijkstra_alg.py
@@ -0,0 +1,64 @@
+
+import sys
+
+
+sys.maxsize
+
+
+class Graph:
+
+    def __init__(self, vertices: int):
+        self.V = vertices
+        self.graph = [[0 for _ in range(vertices)]
+                      for _ in range(vertices)]
+
+    def printSolution(self, dist: list):
+        print("Vertex \tDistance from Source")
+        for node in range(self.V):
+            print(node, '\t', dist[node])
+
+    def minDistance(self, dist: list, sptSet: list):
+
+        min = sys.maxsize
+
+        for u in range(self.V):
+            if dist[u] < min and sptSet[u] == False:
+                min = dist[u]
+                min_index = u
+
+        return min_index
+
+    def dijkstra(self, src: int):
+
+        dist = [sys.maxsize] * self.V
+        dist[src] = 0
+        sptSet = [False] * self.V
+
+        for cout in range(self.V):
+
+            x = self.minDistance(dist, sptSet)
+
+            sptSet[x] = True
+
+            for y in range(self.V):
+                if self.graph[x][y] > 0 and sptSet[y] == False and \
+                        dist[y] > dist[x] + self.graph[x][y]:
+                    dist[y] = dist[x] + self.graph[x][y]
+
+        self.printSolution(dist)
+
+
+if __name__ == "__main__":
+    g = Graph(9)
+    g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+               [4, 0, 8, 0, 0, 0, 0, 11, 0],
+               [0, 8, 0, 7, 0, 4, 0, 0, 2],
+               [0, 0, 7, 0, 9, 14, 0, 0, 0],
+               [0, 0, 0, 9, 0, 10, 0, 0, 0],
+               [0, 0, 4, 14, 10, 0, 2, 0, 0],
+               [0, 0, 0, 0, 0, 2, 0, 1, 6],
+               [8, 11, 0, 0, 0, 0, 1, 0, 7],
+               [0, 0, 2, 0, 0, 0, 6, 7, 0]
+               ]
+
+    g.dijkstra(0)
