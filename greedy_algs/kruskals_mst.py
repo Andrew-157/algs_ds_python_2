4750946775b3a9db1e21b989af7522bf0b54c87b
Andrew Subotin <subotinandrey5@gmail.com>
2023-11-21 10:59:55 +0100

    Find operation on the Disjoint Set

--- greedy_algs/kruskals_mst.py
+++ greedy_algs/kruskals_mst.py
@@ -1,10 +1,25 @@
 
-# class to represent a graph
-class Graph:
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = []
+# Find operation on Disjoint Set
 
-    # a function to add an edge to the graph
-    def add_edge(self, u, v, w):
-        self.graph.append([u, v, w])
+
+# Finds the representative of the set
+# that i is an element of
+
+parent: list[int] = []
+
+
+def find(i):
+
+    # if i is the parent of itself
+    if parent[i] == i:
+
+        # then i is the representative
+        # of its set
+        return i
+
+    else:
+        # Else if i is not the parent of
+        # itself, then i is not the
+        # representative of his set. So we
+        # recursively call Find on its parent
+        return find(parent[i])
